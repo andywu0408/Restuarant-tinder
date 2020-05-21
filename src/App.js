9447c77b5ef69bfd09e357736b373e385e222624
Andy Wu <andywu0408@gmail.com>
2020-05-21 15:20:17 -0700

    Implement static homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { Button } from 'antd';
+import homePageBackground from './assets/homePageBackground.jpg';
 
+//NOTE: This is the home screen
 function App() {
   return (
-    <div> HELLO</div>
+    <main
+      style={Styles.mainContainer}
+    >
+      <h1 style={Styles.title}>
+        Welcome to Restaurant Tinder!
+      </h1>
+      <Button
+        type="primary"
+        block
+        style={Styles.startButton}
+      >
+        Start New Game!
+    </Button>
+    </main>
   );
+}
+
+const Styles = {
+  mainContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 60,
+    height: '100vh',
+    backgroundImage: `url(${homePageBackground})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  },
+  startButton: {
+    height: 120,
+    fontSize: 40,
+    backgroundColor: 'transparent',
+    borderColor: 'white',
+    borderWidth: 5,
+  },
+  title: {
+    fontSize: 80,
+    color: 'white',
+    marginBottom: 200,
+  }
 }
 
 export default App;
