e042265c73c3e1189b48da2cad32d42f5268cdc9
mctammer <tmmohamed@ucdavis.edu>
2020-06-07 00:32:56 -0700

    Add files via upload

---
+++ GameRoom.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import TinderCard from 'react-tinder-card';
+import RestaurantCard from './RestaurantCard';
+import { message, Spin } from 'antd';
+
+//FIXME: fetching yelp API + setting restaurant list + setting numCards should be done in
+// the selection screen(not created yet) and passed in as props. 
+// home screen -> selection screen -> gameRoom
+const GameRoom = () => {
+
+
+  const [Restaurants, setRestaurants] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  const getRestaurants = async () => {
+    
+    console.log("Call a post request to get restaurants from server.")
+  }
+  // TODO: if all users select this card, winner formed!
+  const showSuccess = () => {
+    message.success('Successfuly liked the restaurant', 0.5);
+  };
+  // TODO: remove card from DB on failure
+  const showFailure = () => {
+    message.error('Skipped!!!', 0.5);
+  };
+  const onSwipe = (direction) => {
+    if (direction === 'up' || direction === 'down') {
+      return;
+    }
+
+    direction === 'right' ? showSuccess() : showFailure();
+  }
+
+  const onCardLeftScreen = (myIdentifier) => {
+    console.log(myIdentifier + ' left the screen')
+  }
+
+  return (
+    <div
+      style={Styles.mainContainer}
+    >
+      <div style={Styles.titleContainer}>
+        <div style={Styles.title}>
+          Restaurant Tinder's Game Room
+        </div>
+      </div>
+
+      <div style={Styles.cardContainer}>
+        {isLoaded
+          ? (Restaurants.map((restaurant) => (
+            <TinderCard key={restaurant.name} onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen(restaurant.name)} preventSwipe={['up', 'down']}>
+              <RestaurantCard
+                name={restaurant.name}
+                rating={restaurant.rating} numReviews={restaurant.review_count}
+                priceRange={restaurant.price} picURL={restaurant.image_url}
+                numTimesChosen={0} />
+            </TinderCard>
+          )))
+          : (
+            <div style={Styles.noCard}>
+              <Spin size='large'>
+                <h1 style={Styles.noCard}>WAITING FOR GAME TO START...</h1>
+              </Spin>
+            </div>
+          )}
+      </div>
+
+    </div>
+
+  );
+}
+const Styles = {
+  mainContainer: {
+    background: 'linear-gradient(#e66465, #9198e5)',
+    width: '100vw',
+    minHeight: '100vh',
+    overflow: 'hidden',
+    padding: '20px 300px'
+  },
+  noCard: {
+    position: 'fixed', width: 300, padding: 24,
+    bordeRadius: '1px',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white'
+  },
+  title: {
+    // fontFamily: 'Damion, sans-serif',
+    fontSize: 45,
+    color: 'white',
+    top: 0,
+  },
+  cardContainer: {
+    marginTop: 80,
+    // position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  titleContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+};
+
+export default GameRoom;
---
+++ HomePage.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Modal } from 'antd';
+import SearchControl from '../SearchControl';
+import LocSearchBar from './LocSearchBar';
+import './homepage.css';
+
+//NOTE: This is the home screen
+const HomePage = props => {
+  const [Restaurants, setRestaurants] = useState([]);
+  const [selectedFoodTags, setSelectedFoodTags] = useState(["o"]);
+  const [selectedRestaurantTags, setSelectedRestaurantTags] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState("");
+
+  useEffect(() => {
+    console.log("In homepage")
+    console.log(typeof (selectedFoodTags));
+    console.log(selectedFoodTags)
+  }, [selectedFoodTags]);
+
+  useEffect(() => {
+    console.log("In homepage")
+    console.log(typeof (selectedRestaurantTags));
+    console.log(selectedRestaurantTags)
+  }, [selectedRestaurantTags]);
+
+  useEffect(() => {
+    console.log("In homepage")
+    console.log(typeof (selectedLocation));
+    console.log(selectedLocation)
+  }, [selectedLocation]);
+
+  const setupGame = () => {
+    console.log("Sending post request to server with keywords/loc")
+    getRestaurants();
+    // history.push({
+    //   pathname: `/gameroom`,
+    //   state: {
+    //     loc: selectedLocation,
+    //     queryParams: selectedFoodTags.toString() + "," + selectedRestaurantTags.toString(),
+    //     limit: 20,
+    //   }
+    // });
+  };
+
+  const getRestaurants = async () => {
+
+    let queryParams = selectedFoodTags.toString() + "," + selectedRestaurantTags.toString();
+    let lurl = `https://api.yelp.com/v3/businesses/search?categories=
+                  ${queryParams}
+                  &limit=16&location=${selectedLocation}`;
+    console.log('lurl is ' + lurl)
+    let kek = "https://cors-anywhere.herokuapp.com/"
+
+    let url = kek + lurl;
+    //GaS8MVZOoznvBJmkaZgAHxraTNOgmXnfQVffKpt-6WZZGNPSzL4MSzxFes2uD7V4Y-WqW0V_B_kLysY1TBHGShW9_n9O-vTkbSPqDabxNZPBdnFObQDAXes2UazHXnYx
+
+    await fetch(url, {
+      headers: {
+        //TODO: hide API key with .env
+        Authorization: 'Bearer GaS8MVZOoznvBJmkaZgAHxraTNOgmXnfQVffKpt-6WZZGNPSzL4MSzxFes2uD7V4Y-WqW0V_B_kLysY1TBHGShW9_n9O-vTkbSPqDabxNZPBdnFObQDAXes2UazHXnYx',
+      }
+    })
+      // fetch returns a Promise the resolves into the response object
+      .then(response => { return response.json(); })
+      // parse the JSON from the server; response.json also returns a Promise that
+      // resolves into the JSON content
+      .then(gList => {
+        console.log(gList);
+        //setRestaurants(gList.businesses);
+        sendToServer(gList.businesses);
+
+        console.log("Leaving getRestuarants()")
+      });
+  }
+
+  const sendToServer = (restaurantList) => {
+    console.log("Sending this to servers: ", restaurantList)
+    fetch('/restList',{
+      method: 'POST',
+      body: JSON.stringify({
+        Restaurants: restaurantList
+      }),
+      headers: {"Content-Type": "application/json"}
+    })
+    .then(function(response){
+
+      return response.json()
+    }).then(function(body){
+      console.log(body);
+
+    });
+  }
+  const handleClick = () => {
+    setupGame();
+    Modal.info({
+      title: 'Visit the link to start playing! You can also share the link with your friends so they can join!',
+      content: (
+        //TODO: the link in modal is hardcoded. Change it.
+        //FIXME: User beside host can't enter room. Ask TA
+        <a href={`${window.location.href}gameroom`}>{`${window.location.href}gameroom`}</a>
+      ),
+      // onOk() { goToGameRoom() },
+      keyboard: true,
+      width: '50%',
+      centered: true,
+      okText: 'cancel'
+    });
+  };
+
+  return (
+    <main
+      id="mainContainer"
+    >
+      <h1 id="title">
+        Welcome to Restaurant Tinder!
+      </h1>
+      <div id="searchControlContainer">
+        <SearchControl type="restaurants" updateVal={setSelectedRestaurantTags} />
+        <SearchControl type="food" updateVal={setSelectedFoodTags} />
+        <LocSearchBar updateVal={setSelectedLocation} />
+      </div>
+      <Button
+        type="primary"
+
+        ghost
+        id="startButton"
+        onClick={handleClick}
+      >
+        Start New Game!
+      </Button>
+    </main>
+  );
+}
+
+const Styles = {
+  // mainContainer: {
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   padding: 60,
+  //   height: '100vh',
+  //   backgroundImage: `url(${homePageBackground})`,
+  //   backgroundPosition: 'center',
+  //   backgroundSize: 'cover',
+  //   backgroundRepeat: 'no-repeat',
+  // },
+  // searchControlContainer: {
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  // },
+  // startButton: {
+  //   height: 120,
+  //   fontSize: 40,
+  //   borderColor: 'white',
+  //   borderWidth: 5,
+  // },
+  // title: {
+  //   fontSize: 80,
+  //   color: 'white',
+  //   marginBottom: 200,
+  // },
+  modal: {
+    backgroundColor: 'pink'
+  }
+
+}
+
+export default HomePage;
